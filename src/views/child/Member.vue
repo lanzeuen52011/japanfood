f86cbacb31d1ceddc1ce58b3eedc80b7cabf1724
ZiYun Lan <lanzeuen52011@gmail.com>
2023-08-04 13:30:20 +0800

    設定好Register與Login的切換

--- src/views/child/Member.vue
+++ src/views/child/Member.vue
@@ -1,16 +1,34 @@
 <script>
+import Register from "@/components/register.vue";
 import Login from "@/components/login.vue";
+import { ref } from "vue";
 
 export default {
   components: {
+    Register,
     Login,
   },
   setup() {
-    return {};
+    const login = ref(false);
+    const handleLogin = () => {
+      login.value = !login.value;
+      console.log(login.value);
+    };
+    return {
+      handleLogin,
+      login,
+    };
   },
 };
 </script>
 
 <template>
-  <Login style="margin: calc(var(--var-first_for_navbar) + 1rem) auto 10rem" />
+  <Register class="Member" :handleLogin="handleLogin" v-show="!login" />
+  <Login class="Member" :handleLogin="handleLogin" v-show="login" />
 </template>
+
+<style lang="scss" scoped>
+.Member {
+  margin: calc(var(--var-first_for_navbar) + 1rem) auto 10rem;
+}
+</style>
